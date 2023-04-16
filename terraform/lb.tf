d17fea35c7c66f3910a3a4b9841f80fc6cd2b639
elijaholmos <eli.olmos9@gmail.com>
2023-04-15 17:55:10 -0700

    fix(tf): add ssl cert

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -48,12 +48,18 @@ resource "aws_lb" "alb" {
   ]
 }
 
+data "aws_acm_certificate" "cert" {
+  domain   = "crimlog.org"
+  statuses = ["ISSUED"]
+}
+
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 443
   protocol          = "HTTPS"
 
-  ssl_policy = "ELBSecurityPolicy-2016-08"
+  certificate_arn = data.aws_acm_certificate.cert.arn
+  ssl_policy      = "ELBSecurityPolicy-2016-08"
 
   default_action {
     order            = 1
